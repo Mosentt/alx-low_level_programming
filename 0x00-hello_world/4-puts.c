ecb1201dd1639350c1450ddbe5c56e6e15f5afae
Mosentt <mosesola027@gmail.com>
2023-03-18 21:27:27 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\#include <stdio.h>
+			
+			/**
+			 *  * main - Entry point
+			 *   *
+			 *    * Return: Always 0 (Success)
+			 *     */
+			int main(void)
+			{
+				puts("\"Programming is like building a multilingual puzzle");
+					return (0);
+					}"Programming is like building a multilingual puzzle");
 		return (0);
 }
