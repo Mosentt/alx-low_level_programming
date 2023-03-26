94fe6845426564069a23a7b71a4c65103325f679
Mosentt <mcdonaldcole16@gmail.com>
2023-03-26 16:15:44 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,23 @@
 
 /**
  * print_most_numbers - Print the digits from 0 to 9, excluding 2 and 4.
+ * Return: void
  */
 void print_most_numbers(void)
 {
-    int i;
+	int i = 0;
 
-    for (i = 0; i <= 9; i++)
-    {
-        if (i != 2 && i != 4)
-            _putchar(i + '0');
-    }
-    _putchar('\n');
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
 }
