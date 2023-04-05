ff2897547ef3d840c47abe0c62994a89c9750d03
Mosentt <mcdonaldcole16@gmail.com>
2023-04-04 22:08:38 -0700

    learning is a process

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,12 +2,10 @@
 
 /**
  * _strchr - locates a character in a string
- *
  * @c: occurrence of the character
- *
  * @s: in the string
- *
- * Return: a pointer to the first occurrence of the character
+ * Return: a pointer to the first occurrence
+ * of the character
  */
 
 char *_strchr(char *s, char c)
